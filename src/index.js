f09f35093ea6c91caa4dbcc84d6791d186891c3e
mikhailsechin <mikhailsechin@gmail.com>
2024-02-07 13:12:07 +0300

    chore: improve a11y

--- src/index.js
+++ src/index.js
@@ -13,12 +13,17 @@ for (let i = 0; i < accordions.length; i++) {
     accordions.forEach((item, index) => {
       if (index !== i) {
         item.classList.remove("accordion--expanded");
+        item
+          .querySelector(".accordion__button")
+          .setAttribute("aria-expanded", false);
       } else {
         item.classList.toggle("accordion--expanded");
         if (item.classList.contains("accordion--expanded")) {
           servicesInfoBlock.classList.remove("hidden");
+          button.setAttribute("aria-expanded", true);
         } else {
           servicesInfoBlock.classList.add("hidden");
+          button.setAttribute("aria-expanded", false);
         }
       }
     });
